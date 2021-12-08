f138cad7f09c447b70dee95aa1ae9b9b753c4ef6
smione-reply <si.mione@reply.it>
2021-12-08 20:07:03 +0100

    [Activity] - add support to replace to CountryFragment

--- app/src/main/java/com/example/collezioneeuro/ui/activity/ActivityInterface.kt
+++ app/src/main/java/com/example/collezioneeuro/ui/activity/ActivityInterface.kt
@@ -1,7 +1,10 @@
 package com.example.collezioneeuro.ui.activity
 
+import com.example.collezioneeuro.model.CECountry
+
 interface ActivityInterface {
 
     fun replaceFragmentToHomeFragment()
+    fun replaceFragmentToCountryFragment(ceCountry: CECountry)
 
 }
--- app/src/main/java/com/example/collezioneeuro/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/collezioneeuro/ui/activity/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.collezioneeuro.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.collezioneeuro.R
+import com.example.collezioneeuro.model.CECountry
+import com.example.collezioneeuro.ui.fragment.CountryFragment
 import com.example.collezioneeuro.ui.fragment.HomeFragment
 
 class MainActivity : AppCompatActivity(), ActivityInterface {
@@ -17,6 +19,14 @@ class MainActivity : AppCompatActivity(), ActivityInterface {
         val fragment = HomeFragment.newInstance()
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment, fragment, HomeFragment.TAG)
+            .commit()
+    }
+
+    override fun replaceFragmentToCountryFragment(ceCountry: CECountry) {
+        val fragment = CountryFragment.newInstance(ceCountry)
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(CountryFragment.TAG)
+            .replace(R.id.fragment, fragment, CountryFragment.TAG)
             .commit()
     }
 
