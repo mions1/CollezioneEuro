43d37bd1d1eabfb3b5d7480f1a51d880b38f7010
smione-reply <si.mione@reply.it>
2021-12-08 20:34:43 +0100

    [CEFakeRepository] - add images for italian coins

--- app/src/main/java/com/example/collezioneeuro/model/repository/CEFakeRepository.kt
+++ app/src/main/java/com/example/collezioneeuro/model/repository/CEFakeRepository.kt
@@ -1,5 +1,7 @@
 package com.example.collezioneeuro.model.repository
 
+import com.example.collezioneeuro.R
+import com.example.collezioneeuro.model.CECoin
 import com.example.collezioneeuro.model.CECountry
 
 class CEFakeRepository : CERepositoryInterface {
@@ -9,12 +11,25 @@ class CEFakeRepository : CERepositoryInterface {
     }
 
     private val countries: ArrayList<CECountry> = arrayListOf(
-        CECountry("Italia", CECountry.createCoins()),
+        CECountry("Italia", createItalianCoins()),
         CECountry("Spagna", CECountry.createCoins()),
         CECountry("Grecia", CECountry.createCoins()),
         CECountry("Germania", CECountry.createCoins()),
         CECountry("Francia", CECountry.createCoins()),
     )
+
+    private fun createItalianCoins(): ArrayList<CECoin> {
+        val coins = CECountry.createCoins()
+        coins[0].drawableId = R.drawable.italia_001
+        coins[1].drawableId = R.drawable.italia_002
+        coins[2].drawableId = R.drawable.italia_005
+        coins[3].drawableId = R.drawable.italia_010
+        coins[4].drawableId = R.drawable.italia_020
+        coins[5].drawableId = R.drawable.italia_050
+        coins[6].drawableId = R.drawable.italia_100
+        coins[7].drawableId = R.drawable.italia_200
+        return coins
+    }
 
     override suspend fun getCountries(): ArrayList<CECountry> {
         return countries
